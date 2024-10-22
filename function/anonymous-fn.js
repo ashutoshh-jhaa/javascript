d09054ffa9a11aa0c291eec458807b455e16dd86
ashutosh <ashutoshh.dev@gmail.com>
2024-10-23 00:47:10 +0530

    functions in javascript

---
+++ function/anonymous-fn.js
@@ -0,0 +1,6 @@
+//immediately invoked function are invoked without calling them
+//also called iife
+(function greet() {
+  console.log("hello ashutosh");
+})();
+
---
+++ function/return.js
@@ -0,0 +1,14 @@
+//by default the return is undefined if you don't declare it
+function say(message) {
+  ans = message;
+  console.log(ans);
+}
+
+// console.log("ashutosh"+say("hello"));
+// here the function will return undefined so the answer would be ashutoshundefined
+
+function sayhi(message) {
+  ans = message;
+  console.log(ans);
+}
+// if we assign the function to a variable then the return value is always undefined unless it's defined
