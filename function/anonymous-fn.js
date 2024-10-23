69cfa151f3912778b60951a5bb3b41598922ac7e
ash <ashutoshh.dev@gmail.com>
2024-10-23 23:20:01 +0530

    cleared dir

--- function/anonymous-fn.js
+++
@@ -1,6 +0,0 @@
-//immediately invoked function are invoked without calling them
-//also called iife
-(function greet() {
-  console.log("hello ashutosh");
-})();
-
--- function/function-hoisting.js
+++
@@ -1,7 +0,0 @@
-showMe()//function hoisting example
-
-function showMe() {
-  console.log("hoisting example");
-}
-
-// in hoisting the functin is moved to the top of the code before executing them
