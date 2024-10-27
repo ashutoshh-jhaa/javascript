e4ba909b9d0a0943ef0c5378f91c276952b9f056
ash <ashutoshh.dev@gmail.com>
2024-10-27 22:46:23 +0530

    comma operator in js

--- 03.control-flow-statements/comma-operator.js
+++ 03.control-flow-statements/comma-operator.js
@@ -2,16 +2,16 @@
 //two expressions, evaluates them from left to right, and returns the value
 //of the right expression.
 
+//example 1
 let x = 20;
 let y = (x++, x + 1); //left to right => 21 => 21+1 = 22
 console.log(x);
 console.log(y);
 
+//example 2
 let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 let s = "";
 for (let i = 0, j = 1; i < board.length; i++, j++) {
-  //it is also used here
   s += board[i] + " ";
   if (j % 3 == 0) {
     console.log(s);
