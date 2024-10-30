5dcd088c186b00ab8aed6c236b35ee2b113d6119
ash <ashutoshh.dev@gmail.com>
2024-10-30 20:07:32 +0530

    improved formatting and comments

--- 05.objects/inheritance.js
+++ 05.objects/inheritance.js
@@ -1,26 +1,37 @@
+// Defining an object 'user' with basic properties
 const user = {
-  name: "ashutosh",
+  name: "Ashutosh",
   email: "ash@gmail.com",
 };
+
+// Creating 'teacher' with specific properties
 const teacher = {
   makeVideos: true,
 };
+
+// Creating 'teachingSupport' to represent support-related properties
 const teachingSupport = {
-  isAvailabe: false,
+  isAvailable: false,
 };
+
+// Creating 'taSupport' and setting its prototype to 'teachingSupport'
+// using an older approach (__proto__)
 const taSupport = {
-  makeAssignment: "name of any assignment",
+  makeAssignment: "Assignment name",
   fullTime: false,
-  //outdated approach
-  __proto__: teachingSupport, //now ta support has access to teaching support
+  __proto__: teachingSupport, // 'taSupport' inherits properties from 'teachingSupport'
 };
-//outdated approach
-teacher.__proto__ = user;
-//new approach
-Object.setPrototypeOf(teachingSupport, teacher); //teachingSupport has now access to teacher
+
+// Older approach to setting prototypes directly via '__proto__'
+teacher.__proto__ = user; // 'teacher' inherits properties from 'user'
+
+// Recommended approach for setting prototypes in modern JavaScript:
+Object.setPrototypeOf(teachingSupport, teacher); // 'teachingSupport' inherits from 'teacher'
 
 /*
- *if there lies a situation where we want to a single object or information
- with  other objects we use inheritance 
- basically means that it will inherit the property of another object
+ * Key Concepts:
+ * - Prototype Inheritance: Allows objects to share properties or behaviors with other objects.
+ * - __proto__: Outdated syntax for setting prototypes; avoid using it in modern code.
+ * - Object.setPrototypeOf: Preferred method for defining inheritance in modern JavaScript.
+ * - Inheritance is useful when multiple objects need shared properties or behaviors.
  */
