283e326d46ae5e93c48cd7d07020d485fd93b5d5
ash <ashutoshh.dev@gmail.com>
2024-11-03 12:35:04 +0530

    classes in js

---
+++ 06.classes/basics.js
@@ -0,0 +1,27 @@
+/* Before ES6, JavaScript had no concept of classes.
+ * To mimic a class, you often use the constructor/prototype
+ * classes are syntactic sugar over the prototypal inheritance
+ * in other words class are just special function
+ */
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+}
+let ash = new Person("Ashutosh Jha");
+console.log(typeof Person); //function
+console.log(ash instanceof Person); //true
+console.log(ash instanceof Object); //true 
+
+//section 2: class vs custom type
+//first : class declarations are not hoisted like function declarations
+//if you keep this code above the class declaration it will throw reference error
+let john = new Person("John Doe");
+
+// second : all the code inside a class automatically executes in strict mode
+// third : class method are non-enumerable
+// fourth : calling class constructor without new will result in error
