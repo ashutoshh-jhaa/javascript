dfd417ad7f26f91202aa050ccb7f85b62711ffe9
ash <ashutoshh.dev@gmail.com>
2024-11-20 19:26:08 +0530

    basics of dom

---
+++ 11.js-dom/basics.js
@@ -0,0 +1,3 @@
+//the web browser uses dom to represent the html  document internally additionaly it provides a
+//set of functions and methods to modify the document programmaticaly
+//These functions and methods are often called DOM Application Programming Interfaces or DOM API.
